71006538a87795beadee3a9acf0161544b322959
Eric Normand <ericwnormand@gmail.com>
2019-01-30 16:28:10 -0600

    Update deps

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "https://purelyfunctional.tv/core-async-patterns"
   :license {:name "CC0 1.0 Universal (CC0 1.0) Public Domain Dedication"
             :url "http://creativecommons.org/publicdomain/zero/1.0/"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/core.async "0.2.374"]])
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/core.async "0.4.490"]])
